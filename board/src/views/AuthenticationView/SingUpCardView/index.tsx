edebbc6ddb3c89cf0a616ab3b237391aa1c6f94f
Kim <rlagusxo96652@naver.com>
2023-02-08 17:38:19 +0900

    event set 함수 공부

--- board/src/views/AuthenticationView/SingUpCardView/index.tsx
+++ board/src/views/AuthenticationView/SingUpCardView/index.tsx
@@ -3,10 +3,15 @@ import { Dispatch, SetStateAction, useState } from 'react'
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import Visibility from '@mui/icons-material/Visibility';
 import { useSignUpStore } from 'src/stores';
+import FollowTheSignsIcon from '@mui/icons-material/FollowTheSigns';
+
+
+
 
 
 function FirstPage(){
     const{setEmail,setpassword,setpasswordCheck} = useSignUpStore();
+    const{email,password,passwordCheck} = useSignUpStore();
 
     const [showPassword,setshowPassword] = useState<boolean>(false);
     const [showPasswordCheck,setshowPasswordCheck] = useState<boolean>(false);
@@ -27,6 +32,7 @@ function FirstPage(){
                                 </IconButton>
                             </InputAdornment>
                         }
+                        value={password}
                         onChange={(event)=>setpassword(event.target.value)}
                     />
                 </FormControl>
@@ -45,6 +51,7 @@ function FirstPage(){
                                 </IconButton>
                             </InputAdornment>
                         }
+                        value={passwordCheck}
                         onChange={(event)=>setpasswordCheck(event.target.value)}
                     />
                 </FormControl>
@@ -53,9 +60,35 @@ function FirstPage(){
 }
 
 function SecondPage(){
+    const {nickName,telNumber,address,addressDetail} = useSignUpStore();
+    const {setnickName,settelNumber,setaddress,setaddressDetail} = useSignUpStore();
+
     return(
-        <Box>두번째 페이지</Box>
-    )
+      <Box>
+        <TextField sx={{mt:'40px'}} fullWidth label='닉네임*' variant='standard' value={nickName} onChange={(event)=>setnickName(event.target.value)}/>
+        <TextField sx={{mt:'40px'}} fullWidth label='휴대폰 번호*' variant='standard' value={telNumber} onChange={(event)=>settelNumber(event.target.value)}/>
+
+        <FormControl fullWidth variant='standard' sx={{mt:'40px'}}>
+            <InputLabel>주소*</InputLabel>
+            <Input type="text" endAdornment={
+                <InputAdornment position ="end">
+                    <IconButton>
+                        <FollowTheSignsIcon/>
+                    </IconButton>
+                </InputAdornment>
+
+            }
+            value={address}
+            
+            onChange={(event)=>setaddress(event.target.value)}
+            
+            />
+        </FormControl>
+
+        <TextField sx={{mt:'40px'}} fullWidth label='상세주소*' variant='standard' value={addressDetail} onChange={(event)=>setaddressDetail(event.target.value)}/>
+            
+      </Box>  
+    );
 }
 
 interface Props{
@@ -68,18 +101,62 @@ export default function SignUpCardView({setLoginView}:Props) {
 
     const [page,setPage] = useState<number>(1);
 
-    const {} = useSignUpStore();
+    const {email,password,passwordCheck} = useSignUpStore();
 
-    const onNextButtonHandler = ()=>{
+    const{nickName,telNumber,address,addressDetail} = useSignUpStore();
+
+    const onNextButtonHandler = ()=>{  //! {} 이므로 조건문 사용 가능 
         //todo : 이메일 /비밀번호/비밀번호 확인 검증 
+        // ? 해당 문자열 변수가 빈값인지 확인 
+        // ? 1. 해당 변수 == '';
+        // ? 2. 해당 변수의 길이 == 0 ;
+        if(!email || !password || !passwordCheck){
+            alert('모든 값을 입력하세요');
+            return;
+        }
+        if(password!==passwordCheck){
+            alert('비밀번호가 서로 다릅니다 ')
+            return; 
+        }
+
+
         // todo : 검증이 실패하면 return 
         // todo : 검증이 성공하면 page 변경 
         setPage(2);
     }
 
+    const onSignUpHandler = () =>{
+        if( !email || !password || !passwordCheck){
+            alert('모든 값을 입력하세요');
+            setPage(1);
+            return;
+        }
+        if(!nickName || !telNumber || !address || !addressDetail){
+            alert('모든 값을 입력하세요');
+            setPage(2);
+            return;
+        }
+        
+        if(password!==passwordCheck){
+            alert('비밀번호를 확인해 주세요.');
+            setPage(1);
+            return;
+
+        }
+
+        alert('회원 가입 완료!');
+
+        const data ={
+            email,password,nickName,address,telNumber,addressDetail
+        }
+
+        console.log(data);
+    }
+
 
      return(
         <Box display='flex' sx={{height:'100%',flexDirection:'column',justifyContent:'space-between'}} >
+            
             <Box>
                 <Box display='flex' sx={{justifyContent:'space-between'}}>
                     {/* 가장 먼저 가로로든 세로로든 가로 세로 길이를 먼저 설정해 놔야한다 그래야 justeiftcontents가 먹힌다  */}
@@ -91,7 +168,7 @@ export default function SignUpCardView({setLoginView}:Props) {
             </Box>       
             <Box>
                 {page ===1 && (<Button fullWidth variant = 'contained' size='large' sx={{mb:'20px'}} onClick={onNextButtonHandler}>다음 단계</Button>)}
-                {page ===2 && (<Button fullWidth variant='contained' size = 'large' sx={{mb:'20px'}} onClick={()=>{setPage(1)}}>회원가입</Button>)}
+                {page ===2 && (<Button fullWidth variant='contained' size = 'large' sx={{mb:'20px'}} onClick={onSignUpHandler}>회원가입</Button>)}
                 
                 <Typography variant='h5' fontWeight={'900'}>이미 계정이 있으신가요?
                     <Typography component={'span'} fontWeight={900} onClick={()=>setLoginView(true)}> 로그인</Typography>
