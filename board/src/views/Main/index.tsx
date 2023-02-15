7428d111d61ceecb349a14504273db6f4594f93e
Kim <rlagusxo96652@naver.com>
2023-02-15 14:51:36 +0900

    어딘 가로 url 이동 하려면 navigate()함수

---
+++ board/src/views/Main/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+export default function Main() {
+  return (
+    <Box sx={{pl:'120px',pr:'120px'}}>
+
+
+
+    </Box>
+  )
+}
--- board/src/views/NavigationBar/index.tsx
+++ board/src/views/NavigationBar/index.tsx
@@ -9,71 +9,54 @@ import InputBase from '@mui/material/InputBase';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import { red } from '@mui/material/colors';
+import { Button } from '@mui/material';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(1),
-    width: 'auto',
-  },
-}));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
-      },
-    },
-  },
-}));
 
 export default function NavigationBar() {
-  return (
-    <Box sx={{ flexGrow: 1,pr:"120px",pl:"120px" }}>
+
+  const navigator = useNavigate();
+  const path = useLocation();
+
+  console.log(path.pathname);
+
+    return (
+    <Box sx={{ flexGrow: 1, pr: "120px", pl: "120px" }}>
       <AppBar position="static">
         <Toolbar>
           
+
+            <Typography
+              variant="h6"
+              noWrap   //^noWrap: 이 속성은 텍스트가 컨테이너의 너비를 초과하는 경우 텍스트가 여러 줄로 줄 바꿈되지 않도록 합니다.
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}//^라이브러리를 사용하여 컴포넌트의 스타일을 설정하는 스타일링 속성입니다
+              onClick={()=>navigator('/')}   // 클릭하면 localhost:3000으로(기본으로) 돌아오게 만든다 
+              // navigator()함수 사용 해서 해당 url로 이동 
+            >
+              Hoons Board
+
+            </Typography>
+
           
-          <Typography
-            variant="h6"
-            noWrap   //^noWrap: 이 속성은 텍스트가 컨테이너의 너비를 초과하는 경우 텍스트가 여러 줄로 줄 바꿈되지 않도록 합니다.
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}//^라이브러리를 사용하여 컴포넌트의 스타일을 설정하는 스타일링 속성입니다
-          >
-            Hoons Board
-            
-          </Typography>
-            <IconButton color ={"default"}>
+
+
+          
+          <Box>
+            <IconButton color={"default"}>
               {/* IconButton으로 인해 아이콘 모양이 버튼 클릭도 가능해 진다 */}
               <SearchIcon />
               {/* SearchIcon이 클릭이 가능하게 된다  */}
             </IconButton>
+            {/* path.name이 /auth이면 로그인 버튼 나오게  */}
+            {path.pathname!=='/auth'&&(<Button variant='contained' color='secondary' onClick={()=>navigator('/auth')}>로그인</Button>)}
+            {/*버튼의 variant의 contained를 하면 색상이 가득 찬 버튼 만들기  */}
+            {/* 버튼 클릭했을 때 url 창에 주소 추가해 이동 할려면 (/auth) ==> (Linkto +) useNavigate() 사용  */}
             
+
+          </Box>
+
         </Toolbar>
       </AppBar>
     </Box>
