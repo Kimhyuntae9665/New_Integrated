36ea533f9f879dd4dd9030f04c810b4f76ba2cef
Kim <rlagusxo96652@naver.com>
2023-03-09 17:47:06 +0900

    암호화 토큰

---
+++ board/src/main/java/com/koreait/board/provider/TokenProvider.java
@@ -0,0 +1,41 @@
+package com.koreait.board.provider;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class TokenProvider {
+
+    public String create(){
+        // ^ 현재 시간을 Date 타입으로 바꿔준다 
+        // ^ 현재 시간에 1시간 더해진 시간이 expireDate에 저장 된다 
+        Date expireDate = Date.from(Instant.now().plus(1,ChronoUnit.HOURS));
+        // ^ 줄이 끄어지는 건 조만간 없어진다는 뜻 (1시간 뒤에 삭제 된다 )
+
+        String jwt = 
+                    //  ? Jwts 클래스를 이용해서 JWT 빌드 (생성)
+                    Jwts.builder()
+                    // ? 암호화 알고리즘,암호화 키 
+                    .signWith(SignatureAlgorithm.HS256, "Securekey")
+                    // ? jwt sub의 값을 지정  
+                    .setSubject("id")
+                    // ? jwt iat의 값 지정 (생성 시간)
+                    .setIssuedAt(new Date())
+                    // ? jwt exp의 값 지정 (만료 시간)
+                    .setExpiration(expireDate)
+                    // ? 암호화 알고리즘과 암호화 키를 이용해서 지정한 값들을 토큰으로 변형
+                    .compact();
+
+
+    return jwt;
+        
+        
+    }
+    
+}
