2801fa451701531d693cc319992e2243d837ddc9
Kim <rlagusxo96652@naver.com>
2023-03-09 16:51:43 +0900

    FileController 코드 추가

--- board/src/main/java/com/koreait/board/common/constant/ApiMappingPattern.java
+++ board/src/main/java/com/koreait/board/common/constant/ApiMappingPattern.java
@@ -1,6 +1,9 @@
 package com.koreait.board.common.constant;
 
 public class ApiMappingPattern {
+    
     public static final String HR = "/apis/hr";
     public static final String DEPARTEMENT = "/apis/departement";
+
+    public static final String FILE = "/file";
 }
