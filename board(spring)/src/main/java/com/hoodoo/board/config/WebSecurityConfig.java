d4f3488e9b270e07561cecaf8e3a6af25a44047f
Kim <rlagusxo96652@naver.com>
2023-03-16 17:40:09 +0900

    화이트리스트/블랙리스트

--- board(spring)/src/main/java/com/hoodoo/board/config/WebSecurityConfig.java
+++ board(spring)/src/main/java/com/hoodoo/board/config/WebSecurityConfig.java
@@ -32,9 +32,11 @@ public class WebSecurityConfig {
                                                             //  ^ 이 URL 들은 Token이 없어도 허용 
                                                             // ^ 다른 것들은 Token이 있어야 접근가능 
                                                             // ^ 없으면 401 뜬다 
-            .authorizeRequests().antMatchers("/auth/**","/file/**").permitAll()
-            .antMatchers(HttpMethod.GET,"/api/board/**").permitAll()
+            .authorizeRequests()        //! 공통된 부분이 있으면 상세한 놈이 앞으로 오도록 
             .antMatchers("/api/board/my-list").authenticated()
+            .antMatchers("/auth/**","/file/**").permitAll()
+                                                        // ! 공통된 부분이 있으면 범위가 넓은 놈들이 뒤로 가게 상세한 놈들이 앞으로  
+            .antMatchers(HttpMethod.GET,"/api/board/**").permitAll()
             .anyRequest().authenticated().and()
             .exceptionHandling().authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
 
